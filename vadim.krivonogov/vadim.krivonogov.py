18013d9fa21deced26228b29d48c6178362d2cb2
opilane <opilane@HP-CZC2349HTS>
2023-02-03 13:42:40 +0200

    asdas

--- vadim.krivonogov/vadim.krivonogov.py
+++ vadim.krivonogov/vadim.krivonogov.py
@@ -10,4 +10,17 @@ list.sort(reverse=True)
 
 print(list)
 
-
+#isikukood
+while True:
+    ik=input("Anna isikukood:") #str
+    if len(ik)!=11:
+        print("Liiga palju või liiga vähe sümboleid. Sisesta veel kord: ")
+    else:
+            print("Isikukoodi kontroll")
+            ik_list=list(ik)
+            sl=int(ik_list[0]) #"1"->1
+            if sl not in [1,2,3,4,5,6]:
+                print("Esimene sümbol ei ole õige")
+            else: 
+                print("Esimine sümbol on õige")
+            
